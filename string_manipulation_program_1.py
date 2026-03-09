a341c4cf9bcec058799ec9d1c33b15101cf808f1
yanab <yanaborja610@gmail.com>
2026-03-09 18:24:31 +0800

    Input text and verifying its length before

---
+++ string_manipulation_program_1.py
@@ -0,0 +1,10 @@
+# Program 1: remove the space characters at the end of the string
+
+# For entering texts
+text_input = input("Enter any word: ")
+
+# Verifying length of text before
+print("Length Before:", len(text_input))
+
+# Checks and removes the last character until there are no spaces remaining
+
