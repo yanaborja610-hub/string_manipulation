e5e3db9f1a047d5e5e6bebbb2544455cf580dfcb
yanab <yanaborja610@gmail.com>
2026-03-09 18:32:47 +0800

    Main loop that checks and removes extra space after the word given

--- string_manipulation_program_1.py
+++ string_manipulation_program_1.py
@@ -7,4 +7,8 @@ text_input = input("Enter any word: ")
 print("Length Before:", len(text_input))
 
 # Checks and removes the last character until there are no spaces remaining
+while text_input[-1] == " ":
+    text_input = text_input[:-1]
+
+
 
