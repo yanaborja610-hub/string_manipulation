348f88db717da1e3d57c4111887fba4125324099
yanab <yanaborja610@gmail.com>
2026-03-09 18:42:19 +0800

    Adding further verificaton by using the list() function

--- string_manipulation_program_1.py
+++ string_manipulation_program_1.py
@@ -5,6 +5,7 @@ text_input = input("Enter any word: ")
 
 # Verifying length of text before
 print("Length Before:", len(text_input))
+print(list(text_input))
 
 # Checks and removes the last character until there are no spaces remaining
 while text_input[-1] == " ":
@@ -12,6 +13,7 @@ while text_input[-1] == " ":
 
 # Verifying length of the text after
 print("Length After:", len(text_input))
+print(list(text_input))
 
 # Displaying the word with no space characters at the end of the string
 print(text_input)
