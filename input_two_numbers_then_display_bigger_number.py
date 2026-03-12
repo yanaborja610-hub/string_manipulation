8705dff90bab5fb398fe7165c2de93ceed6d3ec8
yanab <yanaborja610@gmail.com>
2026-03-12 11:13:47 +0800

    Created input function

---
+++ input_two_numbers_then_display_bigger_number.py
@@ -0,0 +1,5 @@
+# Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# Input of numbers
+first_number = float(input("First Number: "))
+second_number = float(input("Second Number: "))
