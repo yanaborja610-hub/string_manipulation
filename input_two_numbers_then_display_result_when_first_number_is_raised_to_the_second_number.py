f21cde5b424c9f0c0971022150ee9a70e1b2af5b
yanab <yanaborja610@gmail.com>
2026-03-12 11:42:29 +0800

    Input/Entering of numbers

--- input_two_numbers_then_display_result_when_first_number_is_raised_to_the_second_number.py
+++ input_two_numbers_then_display_result_when_first_number_is_raised_to_the_second_number.py
@@ -1,1 +1,5 @@
 # Create a program that ask user to input 2 numbers.Print the result when the first number is raised to the second number.
+
+# Input of numbers
+first_number = float(input("Enter a number: "))
+second_number = float(input("Enter another number: "))
