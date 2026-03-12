389e08eee005f707aa8fcce766e8b38ad052c427
yanab <yanaborja610@gmail.com>
2026-03-12 12:19:38 +0800

    Using for loop to display numbers from 0-100 except numbers ending in zero

--- prints_numbers_from_0_to_100_except_numbers_ending_in_zero.py
+++ prints_numbers_from_0_to_100_except_numbers_ending_in_zero.py
@@ -1,1 +1,6 @@
 # Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# Logic of the program then displaying it
+for i in range(0, 101):
+    if i % 10 != 0:
+        print(i)
