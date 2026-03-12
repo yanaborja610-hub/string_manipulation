4dfaf0d36cc97311836b4f2a1321df1eea60de7f
yanab <yanaborja610@gmail.com>
2026-03-12 11:17:59 +0800

    The logic of displaying the bigger number

--- input_two_numbers_then_display_bigger_number.py
+++ input_two_numbers_then_display_bigger_number.py
@@ -3,3 +3,9 @@
 # Input of numbers
 first_number = float(input("First Number: "))
 second_number = float(input("Second Number: "))
+
+# Logic of displaying the bigger number
+if first_number > second_number:
+    print(f"The bigger number is the first number: {first_number}")
+else:
+    print(f"The bigger number is the second number: {second_number}")
