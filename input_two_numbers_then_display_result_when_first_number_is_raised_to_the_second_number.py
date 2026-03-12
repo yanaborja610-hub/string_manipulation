bc2388e8d597740ae38a0ba4a0a149534baa0a01
yanab <yanaborja610@gmail.com>
2026-03-12 11:44:43 +0800

    Logic of the program and displaying the result

--- input_two_numbers_then_display_result_when_first_number_is_raised_to_the_second_number.py
+++ input_two_numbers_then_display_result_when_first_number_is_raised_to_the_second_number.py
@@ -3,3 +3,7 @@
 # Input of numbers
 first_number = float(input("Enter a number: "))
 second_number = float(input("Enter another number: "))
+
+# Logic of the program and displaying the result
+result = first_number ** second_number
+print(f"{first_number} raised to {second_number} = {result}")
