d0ed8b7779898391583605c0c147e6ffeee9594e
yanab <yanaborja610@gmail.com>
2026-03-13 23:45:09 +0800

    Displaying numbers from 0-100 except numbers ending in 0 or 5 using for loop

--- prints_all_numbers_from_0_to_100_except_numbers_ending_in_zero_or_five.py
+++ prints_all_numbers_from_0_to_100_except_numbers_ending_in_zero_or_five.py
@@ -1,1 +1,7 @@
 # Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+# Logic of the program then displaying it
+for i in range(0, 101):
+    if i % 10 != 0:
+        if i % 5 != 0:
+            print(i)
