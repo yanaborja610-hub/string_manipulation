5daa3295255569eeff3bb53d81c6ac1eae0a6390
yanab <yanaborja610@gmail.com>
2026-03-14 00:57:05 +0800

    For the input of 10 numbers using for loop

--- input_10_numbers_display_all_number_that_have_duplicate.py
+++ input_10_numbers_display_all_number_that_have_duplicate.py
@@ -1,1 +1,6 @@
 # Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+# Input of 10 numbers
+numbers = []
+for i in range(10):
+    numbers.append(float(input("Enter a number: ")))
