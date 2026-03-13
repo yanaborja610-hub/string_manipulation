4b68d51cce24cfd1d6b8279a3885d236b1b7a27e
yanab <yanaborja610@gmail.com>
2026-03-14 01:05:13 +0800

    Logic of the program where I used set to make duplicate numbers appear only once, determines the count if it's greater than 1, and displaying all the numbers that have duplicate

--- input_10_numbers_display_all_number_that_have_duplicate.py
+++ input_10_numbers_display_all_number_that_have_duplicate.py
@@ -4,3 +4,8 @@
 numbers = []
 for i in range(10):
     numbers.append(float(input("Enter a number: ")))
+
+# Logic and displaying of the result
+for number in set(numbers): # Using set to make duplicates appear only once
+    if numbers.count(number) > 1: # Will count whether number is greater than 1
+        print(number) # Displaying all number that have duplicate
