dd7396a54689158c5b1590fbd64510cc9d4d6a7e
yanab <yanaborja610@gmail.com>
2026-03-14 00:08:24 +0800

    Displaying the quotient of the two numbers without showing the decimal point

--- input_two_numbers_then_display_their_quotient_without_decimal_point.py
+++ input_two_numbers_then_display_their_quotient_without_decimal_point.py
@@ -3,3 +3,7 @@
 # Input of numbers
 first_number = float(input("First Number: "))
 second_number = float(input("Second Number: "))
+
+# Printing quotient without showing the decimal point
+result = first_number / second_number
+print(f"{first_number} divided by {second_number} = {int(result)}")
