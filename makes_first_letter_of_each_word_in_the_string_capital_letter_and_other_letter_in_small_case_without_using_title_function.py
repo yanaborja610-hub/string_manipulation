8eada4ddace491cf8ef74ec6249e3e35b377b1c9
yanab <yanaborja610@gmail.com>
2026-03-14 10:27:32 +0800

    For the input of any string which is then read to be in lowercase

--- makes_first_letter_of_each_word_in_the_string_capital_letter_and_other_letter_in_small_case_without_using_title_function.py
+++ makes_first_letter_of_each_word_in_the_string_capital_letter_and_other_letter_in_small_case_without_using_title_function.py
@@ -1,1 +1,5 @@
 # Makes all first letter of each word in the string, capital letter. And all other letter in small case without using title() function.
+
+# Input of string
+print("Type anything in random casing")
+name = input("Input: ").lower() # Reads string all in lowercase
