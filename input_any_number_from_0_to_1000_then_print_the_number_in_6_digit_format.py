1bef15f97d23901a94f280e1fba4735cf8d609e5
yanab <yanaborja610@gmail.com>
2026-03-14 09:28:51 +0800

    For the user's input of any number between 0-1000

--- input_any_number_from_0_to_1000_then_print_the_number_in_6_digit_format.py
+++ input_any_number_from_0_to_1000_then_print_the_number_in_6_digit_format.py
@@ -1,1 +1,5 @@
 # Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+# Input of number
+print("Input any number between 0-1000")
+number = (int(input("Enter a number: ")))
