f370881694f861a83e1889d35675a8e242816540
yanab <yanaborja610@gmail.com>
2026-03-20 00:05:32 +0800

    Fixed comments

--- input_two_numbers_print_remainder_when_first_number_is_divided_by_the_second_number.py
+++ input_two_numbers_print_remainder_when_first_number_is_divided_by_the_second_number.py
@@ -1,10 +1,9 @@
-# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
-
+# Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 # Input of numbers
 first_number = float(input("First Number: "))
 second_number = float(input("Second Number: "))
 
-# Printing the remainder without decimal point
+# Printing the remainder
 if second_number != 0:
     remainder = first_number % second_number
     print("Remainder:", remainder)
