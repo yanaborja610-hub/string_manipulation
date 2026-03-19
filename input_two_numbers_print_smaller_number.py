d7d906918c65ac97740eb0b628f0cb6eb5cb94d6
yanab <yanaborja610@gmail.com>
2026-03-19 23:29:01 +0800

    For the input of two numbers

--- input_two_numbers_print_smaller_number.py
+++ input_two_numbers_print_smaller_number.py
@@ -1,1 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print the smaller number.
+
+# Input of numbers
+first_number = float(input("First Number: "))
+second_number = float(input("Second Number: "))
