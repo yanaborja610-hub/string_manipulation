7273c5df1f4da4d8cea374bd7ae81c20deb13e90
yanab <yanaborja610@gmail.com>
2026-03-19 23:39:02 +0800

    Logic of displaying not equal when the numbers are not the same but also taking in mind when numbers are the same

--- input_two_numbers_print_not_equal_when_the_numbers_are_not_the_same.py
+++ input_two_numbers_print_not_equal_when_the_numbers_are_not_the_same.py
@@ -3,3 +3,10 @@
 # Input of numbers
 first_number = float(input("First Number: "))
 second_number = float(input("Second Number: "))
+
+# Logic of displaying not equal when the numbers are not the same
+if first_number != second_number:
+    print("The numbers are NOT EQUAL")
+# When equal
+else:
+    print("The numbers are EQUAL")
