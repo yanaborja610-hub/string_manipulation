21af654e5325ec859080d60a8752e3a235dcd758
yanab <yanaborja610@gmail.com>
2026-03-19 22:29:54 +0800

    For input of two numbers

--- input_two_numbers_print_equal_when_numbers_are_same.py
+++ input_two_numbers_print_equal_when_numbers_are_same.py
@@ -1,1 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+# Input of numbers
+first_number = float(input("First Number: "))
+second_number = float(input("Second Number: "))
