bb665938c9e07740e79051602fc5f4612eb075dd
yanab <yanaborja610@gmail.com>
2026-03-19 23:08:34 +0800

    Added the 'total' variable, logic behind adding all the numbers inputted, and displaying the sum

--- input_10_numbers_print_their_sum.py
+++ input_10_numbers_print_their_sum.py
@@ -2,5 +2,11 @@
 
 # Input of 10 numbers
 numbers = []
+total = 0
 for i in range(10):
     numbers.append(float(input("Enter a number: ")))
+    # For adding all the numbers
+    total += numbers[i]
+
+# Printing the sum of all numbers
+print("The sum of all the numbers are", total)
