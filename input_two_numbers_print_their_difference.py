5f6f0bb7f169f10e35f67f4fa0fd84f527294d47
yanab <yanaborja610@gmail.com>
2026-03-19 23:42:46 +0800

    For the input of two numbers

--- input_two_numbers_print_their_difference.py
+++ input_two_numbers_print_their_difference.py
@@ -1,1 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+# Input of numbers
+first_number = float(input("First Number: "))
+second_number = float(input("Second Number: "))
