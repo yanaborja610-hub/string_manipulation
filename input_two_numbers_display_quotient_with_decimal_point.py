99c81f2ecafcf0a05d3d487708c1e28e13fa0061
yanab <yanaborja610@gmail.com>
2026-03-19 22:51:54 +0800

    For the input of numbers

--- input_two_numbers_display_quotient_with_decimal_point.py
+++ input_two_numbers_display_quotient_with_decimal_point.py
@@ -1,1 +1,5 @@
 # Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+# Input of numbers
+first_number = float(input("First Number: "))
+second_number = float(input("Second Number: "))
