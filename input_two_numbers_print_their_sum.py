ecb0e83240e421cb9308b0abab7312a7d3413173
yanab <yanaborja610@gmail.com>
2026-03-19 22:39:35 +0800

    Getting the sum of the two numbers

--- input_two_numbers_print_their_sum.py
+++ input_two_numbers_print_their_sum.py
@@ -3,3 +3,6 @@
 # Input of numbers
 first_number = float(input("First Number: "))
 second_number = float(input("Second Number: "))
+
+# Getting their sum
+print(f"{first_number} + {second_number} = {first_number + second_number}")
