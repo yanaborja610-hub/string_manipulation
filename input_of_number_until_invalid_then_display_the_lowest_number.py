5e364d617b55389c6c3c49b21a937beb6c12a12d
yanab <yanaborja610@gmail.com>
2026-03-20 09:53:22 +0800

    Adding list for storing numbers, while loop for the user's input until invalid

--- input_of_number_until_invalid_then_display_the_lowest_number.py
+++ input_of_number_until_invalid_then_display_the_lowest_number.py
@@ -1,1 +1,12 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+numbers = [] # For storing numbers
+# While loop to keep on asking until input is invalid
+while True:
+    user_input = input("Enter a number: ")
+
+    try:
+        num = float(user_input)
+        numbers.append(num)
+    except ValueError:
+        break  # Stops when input is invalid
