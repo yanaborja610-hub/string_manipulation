4a9107f05b48750be0ba4ab4e3b2bc6db7ed6bd3
yanab <yanaborja610@gmail.com>
2026-03-20 18:57:20 +0800

    For the input of text and character to find

--- return_the_first_location_of_a_character_in_string_starting_from_last_character.py
+++ return_the_first_location_of_a_character_in_string_starting_from_last_character.py
@@ -1,2 +1,6 @@
 # rindex() return the first location of the function parameter in the string starting from the last character.
 # Create a program that do the same functionality without using rindex() function.
+
+# Input string and character to find
+text = input("Enter text: ")
+target = input("Enter character to find: ")
