d96e0fb2b033db129ae2098b2447a0d2a38b5ccb
yanab <yanaborja610@gmail.com>
2026-03-20 17:27:04 +0800

    For the input of text and suffix to be removed

--- remove_characters_at_the_end_of_string_without_using_removesuffix_function.py
+++ remove_characters_at_the_end_of_string_without_using_removesuffix_function.py
@@ -1,2 +1,6 @@
 # removesuffix() remove the characters at the end of the string that matches the function parameter.
 # Create a program that do the same functionality without using removesuffix() function.
+
+# For the input of text and suffix to be removed
+text = input("Enter text: ")
+suffix = input("Enter suffix to remove: ")
