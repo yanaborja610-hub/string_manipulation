bcc15aaab5699a5c6f8553a972dd5d04a1a5a354
yanab <yanaborja610@gmail.com>
2026-03-20 14:55:36 +0800

    Uploading file

---
+++ remove_characters_at_the_beginning_of_string_without_using_removeprefix_function.py
@@ -0,0 +1,2 @@
+# removeprefix() remove the characters at the beginning of the string that matches the function parameter.
+# Create a program that do the same functionality without using removeprefix() function.
