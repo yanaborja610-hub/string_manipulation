fd2fa42f99ee0080bce789a8907ec58eec9d4fa7
yanab <yanaborja610@gmail.com>
2026-03-20 17:48:02 +0800

    For the input of text and checking whether it is all in lower case

--- checks_if_characters_in_string_is_on_lower_case.py
+++ checks_if_characters_in_string_is_on_lower_case.py
@@ -1,2 +1,7 @@
 # islower() check if all characters of the string is on lower case.
 # Create a program that do the same functionality without using islower() function.
+
+# For the input of text
+print("Checks if text is all in lower casing")
+text = input("Enter text: ")
+text_is_lower = True  # Checks if text is all in lower case
