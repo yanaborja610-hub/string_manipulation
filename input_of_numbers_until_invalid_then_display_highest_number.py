f5d4e345dcd3120796bd6daa000598846cc955c2
yanab <yanaborja610@gmail.com>
2026-03-20 10:39:25 +0800

    Added list for storing values and used while loop for user's input until input is invalid

--- input_of_numbers_until_invalid_then_display_highest_number.py
+++ input_of_numbers_until_invalid_then_display_highest_number.py
@@ -1,2 +1,13 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display the highest number
+
+numbers = [] # For storing numbers
+# While loop to keep on asking until input is invalid
+while True:
+    user_input = input("Enter a number: ")
+
+    try:
+        num = float(user_input)
+        numbers.append(num)
+    except ValueError:
+        break  # Stops when input is invalid
