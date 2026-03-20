de3a12ccc2cf98c81ae323bb74d4f2ff134a3bfa
yanab <yanaborja610@gmail.com>
2026-03-20 16:38:25 +0800

    Tells how many spaces to add, divides spaces evenly for the left side, and the remaining spaces goes to the right side

--- add_space_at_the_beginning_and_end_of_string_to_make_it_centered.py
+++ add_space_at_the_beginning_and_end_of_string_to_make_it_centered.py
@@ -4,3 +4,7 @@
 # For the input of text and desired width
 text = input("Enter text: ")
 width = int(input("Enter desired width: "))
+
+spaces_needed = width - len(text) # Tells how many spaces to add
+left = spaces_needed // 2 # Divides spaces evenly for the left side
+right = spaces_needed - left # Remaining spaces goes to the right side
