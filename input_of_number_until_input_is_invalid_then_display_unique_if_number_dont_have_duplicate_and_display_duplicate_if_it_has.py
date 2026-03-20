43412d935dd0c08e2ae69439979af6fb71e08a75
yanab <yanaborja610@gmail.com>
2026-03-20 09:28:08 +0800

    While loop to keep on asking for user's input until invalid

--- input_of_number_until_input_is_invalid_then_display_unique_if_number_dont_have_duplicate_and_display_duplicate_if_it_has.py
+++ input_of_number_until_input_is_invalid_then_display_unique_if_number_dont_have_duplicate_and_display_duplicate_if_it_has.py
@@ -1,3 +1,9 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display "Unique" after input when the inputted number don't have duplicate.
 # Display "Duplicate" after input when the inputted number have duplicate.
+
+numbers = [] # Storing numbers inputted
+# While loop to keep on asking for input until invalid
+while True:
+    try:
+        num = float(input("Enter number: "))
