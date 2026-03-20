b4e011f87f91abd8a7abeb59b5b6f6a4b4ffb21b
yanab <yanaborja610@gmail.com>
2026-03-20 17:12:17 +0800

    Uploading file

---
+++ makes_all_first_letter_of_each_word_in_string_capital_and_all_other_letters_in_lowercase.py
@@ -0,0 +1,2 @@
+# title() makes all first letter of each word in the string, capital letter. And all other letter in small case.
+# Create a program that do the same functionality without using title() function.
