01a382d84101f584476fa7794224a0cbe33293bd
yanab <yanaborja610@gmail.com>
2026-03-20 16:19:36 +0800

    For the input of text and desired with plus telling how many spaces to add on text

--- add_space_characters_at_the_end_of_string_to_complete_desired_width.py
+++ add_space_characters_at_the_end_of_string_to_complete_desired_width.py
@@ -1,2 +1,9 @@
 # ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter.
 # Create a program that do the same functionality without using ljust() function.
+
+# For the input of text and desired width
+text = input("Enter text: ")
+width = int(input("Enter total width: "))
+
+# Tells how many spaces to add
+spaces_needed = width - len(text)
