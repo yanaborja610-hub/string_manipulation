301b030a693bcec2c6e9e9e73c8316948dd2fda3
yanab <yanaborja610@gmail.com>
2026-03-20 18:04:25 +0800

    For the input of text and prefix

--- checks_if_string_starts_with_the_inputted_prefix.py
+++ checks_if_string_starts_with_the_inputted_prefix.py
@@ -1,2 +1,7 @@
 # startswith() check if the string beginning part matches the function parameter.
 # Create a program that do the same functionality without using startswith() function.
+
+# For input of text and prefix
+print("Checks if the text starts with the inputted prefix")
+text = input("Enter text: ")
+prefix = input("Enter prefix: ")
