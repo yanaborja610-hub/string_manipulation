1e6ac23618699ab07ed9c43b9d4ae9eb63c8b209
yanab <yanaborja610@gmail.com>
2026-03-20 16:18:17 +0800

    Uploading file

---
+++ add_space_characters_at_the_end_of_string_to_complete_desired_width.py
@@ -0,0 +1,2 @@
+# ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter.
+# Create a program that do the same functionality without using ljust() function.
