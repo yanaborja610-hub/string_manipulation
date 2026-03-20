168a715b0f520bbd7c2462aaf538beccd170ab91
yanab <yanaborja610@gmail.com>
2026-03-20 18:17:32 +0800

    Tells how many spaces to add, actually adds the spaces at the beginning to the string, and displaying result

--- add_space_at_the_beginning_of_string_to_complete_desired_width.py
+++ add_space_at_the_beginning_of_string_to_complete_desired_width.py
@@ -4,3 +4,15 @@
 # For the input of text and desired width
 text = input("Enter text: ")
 width = int(input("Enter total width: "))
+
+# Tells how many spaces to add
+spaces_needed = width - len(text)
+
+# Adds the spaces to the string
+if spaces_needed > 0:
+    result = " " * spaces_needed + text
+else:
+    result = text
+
+# Displays result
+print("'" + result + "'")
