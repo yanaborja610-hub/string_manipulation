6b742f7f7da2c655fb11c1ad8fe2ddc6b7f3b732
yanab <yanaborja610@gmail.com>
2026-03-20 17:37:57 +0800

    Added variables for input of text and empty string for later use

--- converts_all_characters_of_string_to_upper_casing.py
+++ converts_all_characters_of_string_to_upper_casing.py
@@ -1,2 +1,6 @@
 # upper() converts all characters of the string into upper case.
 # Create a program that do the same functionality without using upper() function.
+
+# For the input of text
+text = input("Enter text in lower case: ")
+result = "" # Empty string for later use
