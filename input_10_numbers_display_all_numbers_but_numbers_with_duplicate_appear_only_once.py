f50bbc73102992e9f5be57e0ece15a6e80feb185
yanab <yanaborja610@gmail.com>
2026-03-20 09:15:57 +0800

    For the input of 10 numbers

--- input_10_numbers_display_all_numbers_but_numbers_with_duplicate_appear_only_once.py
+++ input_10_numbers_display_all_numbers_but_numbers_with_duplicate_appear_only_once.py
@@ -1,1 +1,6 @@
 # Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+# Input of 10 numbers
+numbers = []
+for i in range(10):
+    numbers.append(float(input("Enter a number: ")))
