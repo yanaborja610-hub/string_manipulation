4fdf4bbdae938573b3163a592dbe8822e8e4f6a4
yanab <yanaborja610@gmail.com>
2026-03-20 15:57:56 +0800

    For the input of text and suffix

--- checks_if_string_end_part_matches_the_function_parameter.py
+++ checks_if_string_end_part_matches_the_function_parameter.py
@@ -1,2 +1,7 @@
 # endswith() check if the string end part matches the function parameter.
 # Create a program that do the same functionality without using endswith() function.
+
+# For the input of text and suffix
+print("Checks if the text end with the inputted parameter")
+text = input("Enter text: ")
+suffix = input("Enter suffix: ")
