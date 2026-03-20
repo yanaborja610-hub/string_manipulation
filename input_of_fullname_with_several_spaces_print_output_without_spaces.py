0bf3fd9b2487508f620b237612a8b507c8ca4179
yanab <yanaborja610@gmail.com>
2026-03-20 13:24:21 +0800

    For the input of name and displaying output without spaces in the beginning

--- input_of_fullname_with_several_spaces_print_output_without_spaces.py
+++ input_of_fullname_with_several_spaces_print_output_without_spaces.py
@@ -1,2 +1,9 @@
 # Create a program that ask the user to input their fullname with several space characters at the beginning.
 # Print the input without the spaces in the beginning.
+
+# For the input of name
+name = input("Input your full name with several spaces: ")
+
+# For output
+name_without_spaces = name.lstrip()
+print("Output without spaces:", name_without_spaces)
