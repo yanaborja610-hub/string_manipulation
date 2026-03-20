6e0c5ff3a55827cf70aea5285c9cbb2d1928851f
yanab <yanaborja610@gmail.com>
2026-03-20 16:54:34 +0800

    For the input of text

--- reverse_casing_of_each_character_of_the_string.py
+++ reverse_casing_of_each_character_of_the_string.py
@@ -1,2 +1,5 @@
 # swapcase() reverse the casing of each of the character of the string.
 # Create a program that do the same functionality without using swapcase() function.
+
+# For the input of text
+text = input("Enter text in random casing: ")
