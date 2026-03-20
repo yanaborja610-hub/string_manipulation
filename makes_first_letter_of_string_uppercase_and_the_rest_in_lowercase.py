94b33cb56cd315fef12377d84ec2dc8daaebbaaa
yanab <yanaborja610@gmail.com>
2026-03-20 17:03:16 +0800

    Uploading file

---
+++ makes_first_letter_of_string_uppercase_and_the_rest_in_lowercase.py
@@ -0,0 +1,2 @@
+# capitalize() makes the first letter of the string, capital letter. And all other letter in small case.
+# Create a program that do the same functionality without using capitalize() function.
