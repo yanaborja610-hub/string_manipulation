3ba26e29c2604af3fe190cae4c919d5a25341be9
yanab <yanaborja610@gmail.com>
2026-03-20 14:56:48 +0800

    For the input of text and removing prefixes

--- remove_characters_at_the_beginning_of_string_without_using_removeprefix_function.py
+++ remove_characters_at_the_beginning_of_string_without_using_removeprefix_function.py
@@ -1,2 +1,6 @@
 # removeprefix() remove the characters at the beginning of the string that matches the function parameter.
 # Create a program that do the same functionality without using removeprefix() function.
+
+# Input of text and removing prefix
+text = input("Enter any text: ")
+remove_prefix = input("Enter prefix to remove (must include the first character of your text): ")
