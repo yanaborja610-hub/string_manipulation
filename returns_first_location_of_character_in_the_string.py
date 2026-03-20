3adfb997a37a1f3cd52755ce7954750845d0bdab
yanab <yanaborja610@gmail.com>
2026-03-20 18:44:52 +0800

    Input of text and character to find

--- returns_first_location_of_character_in_the_string.py
+++ returns_first_location_of_character_in_the_string.py
@@ -1,2 +1,6 @@
 # index() return the first location of the function parameter in the string.
 # Create a program that do the same functionality without using index() function.
+
+# Input string and character to find
+text = input("Enter text: ")
+target = input("Enter character to find: ")
