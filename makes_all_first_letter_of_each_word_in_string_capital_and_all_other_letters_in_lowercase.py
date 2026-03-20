634c965355088c224ff59e4afdfe4ec448558bfd
yanab <yanaborja610@gmail.com>
2026-03-20 17:13:14 +0800

    For the input of text

--- makes_all_first_letter_of_each_word_in_string_capital_and_all_other_letters_in_lowercase.py
+++ makes_all_first_letter_of_each_word_in_string_capital_and_all_other_letters_in_lowercase.py
@@ -1,2 +1,5 @@
 # title() makes all first letter of each word in the string, capital letter. And all other letter in small case.
 # Create a program that do the same functionality without using title() function.
+
+# For input of text
+text = input("Enter text: ")
