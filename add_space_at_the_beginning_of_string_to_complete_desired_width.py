59f000864a4c2ea6508b482db9b5dce1c4f9c2fd
yanab <yanaborja610@gmail.com>
2026-03-20 18:15:18 +0800

    For the input of text and desired width

--- add_space_at_the_beginning_of_string_to_complete_desired_width.py
+++ add_space_at_the_beginning_of_string_to_complete_desired_width.py
@@ -1,2 +1,6 @@
 # rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter.
 # Create a program that do the same functionality without using rjust() function.
+
+# For the input of text and desired width
+text = input("Enter text: ")
+width = int(input("Enter total width: "))
