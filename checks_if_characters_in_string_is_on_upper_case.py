d4941bcb114a7590339c89641f77c70c66a35048
yanab <yanaborja610@gmail.com>
2026-03-20 15:42:04 +0800

    For the input of text and checking whether the text is all in upper casing

--- checks_if_characters_in_string_is_on_upper_case.py
+++ checks_if_characters_in_string_is_on_upper_case.py
@@ -1,2 +1,7 @@
 # isupper() check if all characters of the string is on upper case.
 # Create a program that do the same functionality without using isupper() function.
+
+# For the input of text
+print("Checks if text is all in upper casing")
+text = input("Enter text: ")
+text_is_upper = True  # Checks if text is all in upper case
