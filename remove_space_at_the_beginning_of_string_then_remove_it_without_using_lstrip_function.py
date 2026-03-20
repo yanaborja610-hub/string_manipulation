e1b8e261275827d852fc9133db70452a309c800f
yanab <yanaborja610@gmail.com>
2026-03-20 14:44:31 +0800

    Logic behind removing space at the beginning of string and displaying the text without spaces

--- remove_space_at_the_beginning_of_string_then_remove_it_without_using_lstrip_function.py
+++ remove_space_at_the_beginning_of_string_then_remove_it_without_using_lstrip_function.py
@@ -4,3 +4,9 @@
 # Ask for user's input
 text = input("Input several spaces before adding text: ")
 i = 0 # Initializes count
+
+while i < len(text) and text[i] == " ": # Ensures character is less than the text and checks if a character is a space
+    i += 1 # Skips spaces by moving to the first actual character
+
+# Prints the text starting from 1st character to the end
+print(text[i:])
