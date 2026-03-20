3718843948d4b34b5ff7adde6cef5dc877eb899f
yanab <yanaborja610@gmail.com>
2026-03-20 15:18:33 +0800

    For the input of text and added an empty string for later use

--- converts_all_characters_of_string_to_lower_casing.py
+++ converts_all_characters_of_string_to_lower_casing.py
@@ -1,2 +1,6 @@
 # lower() converts all characters of the string into lower case.
 # Create a program that do the same functionality without using lower() function.
+
+# For the input of text
+text = input("Enter text in capital letters: ")
+result = "" # Empty string for later use
