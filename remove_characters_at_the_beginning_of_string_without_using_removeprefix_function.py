69a6a20a20bd589ea4a730319f0da8264a39a241
yanab <yanaborja610@gmail.com>
2026-03-20 14:59:50 +0800

    Removes characters at the beginning of the string otherwise printing the whole text if remove_prefix input does not include first character of the string

--- remove_characters_at_the_beginning_of_string_without_using_removeprefix_function.py
+++ remove_characters_at_the_beginning_of_string_without_using_removeprefix_function.py
@@ -4,3 +4,9 @@
 # Input of text and removing prefix
 text = input("Enter any text: ")
 remove_prefix = input("Enter prefix to remove (must include the first character of your text): ")
+
+# Removes characters at the beginning of the string
+if text[:len(remove_prefix)] == remove_prefix:
+    print(text[len(remove_prefix):])
+else:
+    print(text)
