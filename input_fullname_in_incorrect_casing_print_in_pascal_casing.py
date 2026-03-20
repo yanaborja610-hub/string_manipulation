41e7351d74a5bad2c99a038cc0ba051265ee9923
yanab <yanaborja610@gmail.com>
2026-03-20 14:21:50 +0800

    For the input of fullname in incorrect casing

--- input_fullname_in_incorrect_casing_print_in_pascal_casing.py
+++ input_fullname_in_incorrect_casing_print_in_pascal_casing.py
@@ -1,2 +1,5 @@
 # Create a program that ask the user to input their fullname in incorrect casing.
 # Print the input in snake case.
+
+# For the input of name
+name = input("Enter your full name in incorrect casing: ")
