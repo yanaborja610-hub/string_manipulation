b8fb6a9503c67efd9fd571bf5ec034742a68aac3
yanab <yanaborja610@gmail.com>
2026-03-20 18:28:12 +0800

    Adding zeros at the beginning to reach the desired width and displaying the result

--- add_zero_at_the_beginning_of_the_string_without_using_zfill_function.py
+++ add_zero_at_the_beginning_of_the_string_without_using_zfill_function.py
@@ -5,3 +5,9 @@
 # For the input of text and desired width
 text = input("Enter text: ")
 width = int(input("Enter total width: "))
+
+# Add zeros at the beginning to reach the desired width
+result = "0" * max(width - len(text), 0) + text
+
+# Display result
+print("'" + result + "'")
