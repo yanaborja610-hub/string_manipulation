3592e4a1e6c37ecdae7fa973a4ed547b80be7805
yanab <yanaborja610@gmail.com>
2026-03-20 14:24:53 +0800

    Change file name to proper and fix some comments

--- input_fullname_in_incorrect_casing_print_in_snake_casing.py
+++ input_fullname_in_incorrect_casing_print_in_snake_casing.py
@@ -4,6 +4,6 @@
 # For the input of name
 name = input("Enter your full name in incorrect casing: ")
 
-# For the output in pascal casing
+# For the output in snake casing
 snake_case = "_".join(name.lower().split())
 print("Output in snake casing:", snake_case)
