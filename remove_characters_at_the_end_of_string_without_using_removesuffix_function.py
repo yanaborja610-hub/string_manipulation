6070588abb9e3311a3f66bf5c3774cd364f46f54
yanab <yanaborja610@gmail.com>
2026-03-20 17:25:51 +0800

    Uploading file

---
+++ remove_characters_at_the_end_of_string_without_using_removesuffix_function.py
@@ -0,0 +1,2 @@
+# removesuffix() remove the characters at the end of the string that matches the function parameter.
+# Create a program that do the same functionality without using removesuffix() function.
