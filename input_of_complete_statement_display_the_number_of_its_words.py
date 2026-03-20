a2f625fcda94324972f890779df31f7042027e42
yanab <yanaborja610@gmail.com>
2026-03-20 13:53:10 +0800

    For the input of a complete statement

--- input_of_complete_statement_display_the_number_of_its_words.py
+++ input_of_complete_statement_display_the_number_of_its_words.py
@@ -1,2 +1,5 @@
 # Create a program that ask the user to input a complete statement.
 # Print the number of words in the input.
+
+# For the input of name
+name = input("Input a complete statement: ")
