6eea5af5555c65855ef29f56e5bc897a80a3d55c
yanab <yanaborja610@gmail.com>
2026-03-20 18:26:47 +0800

    For the input of text and desired width

--- add_zero_at_the_beginning_of_the_string_without_using_zfill_function.py
+++ add_zero_at_the_beginning_of_the_string_without_using_zfill_function.py
@@ -1,3 +1,7 @@
 # zfill() add zero characters at the beginning of the string to
 # complete the number of characters specifies in function parameter.
 # Create a program that do the same functionality without using zfill() function.
+
+# For the input of text and desired width
+text = input("Enter text: ")
+width = int(input("Enter total width: "))
