d7674fa7a75825d1dbb95469be0fa9ad1e10ffa6
yanab <yanaborja610@gmail.com>
2026-03-20 18:36:15 +0800

    Input of text and character to count

--- return_how_many_time_a_character_appear_in_string.py
+++ return_how_many_time_a_character_appear_in_string.py
@@ -1,2 +1,6 @@
 # count() return how many time the function parameter appear in the string.
 # Create a program that do the same functionality without using count() function.
+
+# Input string and character to count
+text = input("Enter text: ")
+characters = input("Enter character to count: ")
