f541133a9b77d6e28e2672366784dcef6a450c0e
yanab <yanaborja610@gmail.com>
2026-03-20 10:49:34 +0800

    Added list for storing numbers and used while loop to keep on asking until input is invalid

--- input_of_numbers_until_invalid_then_display_them_from_highest_to_lowest.py
+++ input_of_numbers_until_invalid_then_display_them_from_highest_to_lowest.py
@@ -1,2 +1,13 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display the number from highest to lowest. Clue: sort() function
+
+numbers = [] # For storing numbers
+# While loop to keep on asking until input is invalid
+while True:
+    user_input = input("Enter a number: ")
+
+    try:
+        num = float(user_input)
+        numbers.append(num)
+    except ValueError:
+        break  # Stops when input is invalid
