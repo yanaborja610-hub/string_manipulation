1dee0d2a5c7dce086220e69ab09b534f77c65e27
yanab <yanaborja610@gmail.com>
2026-03-20 08:42:50 +0800

    Starts with the first odd number, adds 2 from that, and displays odd numbers from 1 to 100

--- prints_all_odd_numbers_from_0_to_100_using_while_loop.py
+++ prints_all_odd_numbers_from_0_to_100_using_while_loop.py
@@ -1,1 +1,7 @@
 # Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+num = 1 # Starts with the first odd number 1
+
+while num <= 100: # Loop stops at 100
+    print(num)
+    num += 2 # Adds 2 from 1 until it reaches 100
