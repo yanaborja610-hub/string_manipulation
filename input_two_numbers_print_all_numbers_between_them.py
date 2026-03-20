e56d792c8bb507d26af7b22231c3eb9890220cba
yanab <yanaborja610@gmail.com>
2026-03-20 09:00:35 +0800

    Displaying all the numbers between two numbers in ascending oder if first number is bigger than the second number and vice versa

--- input_two_numbers_print_all_numbers_between_them.py
+++ input_two_numbers_print_all_numbers_between_them.py
@@ -3,3 +3,15 @@
 # Input of numbers
 first_number = float(input("First Number: "))
 second_number = float(input("Second Number: "))
+
+# Displaying all the numbers between the 2 numbers in ascending or descending order
+if first_number < second_number:
+    i = first_number + 1
+    while i < second_number:
+        print(i)
+        i += 1
+else:
+    i = first_number - 1
+    while i > second_number:
+        print(i)
+        i -= 1
