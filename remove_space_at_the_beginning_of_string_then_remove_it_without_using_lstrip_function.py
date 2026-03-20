06f1fbd82d8580940416c8c8e8fb8f8a904a8d0f
yanab <yanaborja610@gmail.com>
2026-03-20 14:42:34 +0800

    For the user's input and to initialize count of characters

--- remove_space_at_the_beginning_of_string_then_remove_it_without_using_lstrip_function.py
+++ remove_space_at_the_beginning_of_string_then_remove_it_without_using_lstrip_function.py
@@ -1,2 +1,6 @@
 #  lstrip() remove the space characters at the beginning of the string.
 #  Create a program that do the same functionality without using lstrip() function.
+
+# Ask for user's input
+text = input("Input several spaces before adding text: ")
+i = 0 # Initializes count
