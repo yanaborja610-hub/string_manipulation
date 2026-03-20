32abce7574876dfaff468617f2ff66a6deba0a8a
yanab <yanaborja610@gmail.com>
2026-03-20 16:35:13 +0800

    Uploading file

---
+++ add_space_at_the_beginning_and_end_of_string_to_make_it_centered.py
@@ -0,0 +1,2 @@
+# center() add space characters at the beginning and at the end of the string to print the string at the center.
+# Create a program that do the same functionality without using center() function.
