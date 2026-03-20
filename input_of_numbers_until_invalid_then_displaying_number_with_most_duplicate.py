3d31610346268ddcf478fd3809610220b570a0e9
yanab <yanaborja610@gmail.com>
2026-03-20 10:28:38 +0800

    Added list for storing numbers and used while loop for keeping input going until input is invalid

--- input_of_numbers_until_invalid_then_displaying_number_with_most_duplicate.py
+++ input_of_numbers_until_invalid_then_displaying_number_with_most_duplicate.py
@@ -1,2 +1,10 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display the number with the most number of duplicate.
+
+numbers = [] # For storing numbers
+# Using while loop to keep input going until input is invalid
+while True:
+    try:
+        numbers.append(float(input("Enter a number: ")))
+    except ValueError:
+        break # Stops the loop
