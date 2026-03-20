236d70a2ff0fb9a6160c7aa4e0ae0f13eecc3ec6
yanab <yanaborja610@gmail.com>
2026-03-20 15:21:29 +0800

    Used for loop in converting capital letters to small case but also kept in mind that small characters inputted remains unchanged

--- converts_all_characters_of_string_to_lower_casing.py
+++ converts_all_characters_of_string_to_lower_casing.py
@@ -4,3 +4,10 @@
 # For the input of text
 text = input("Enter text in capital letters: ")
 result = "" # Empty string for later use
+
+# Using for loop in converting capital letters to small case
+for characters in text:
+    if 'A' <= characters <= 'Z': # Checks if letters are uppercase
+        result += chr(ord(characters) + 32) # Converts by adding 32 to its ASCII value
+    else:
+        result += characters # Keeps small characters unchanged
