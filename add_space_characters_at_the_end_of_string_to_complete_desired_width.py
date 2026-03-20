a3fddc60385e94e5fbba8d8ef0fb78787c46b17b
yanab <yanaborja610@gmail.com>
2026-03-20 16:21:16 +0800

    Adds spaces to the text to satisfy the desired length and displays result

--- add_space_characters_at_the_end_of_string_to_complete_desired_width.py
+++ add_space_characters_at_the_end_of_string_to_complete_desired_width.py
@@ -7,3 +7,12 @@ width = int(input("Enter total width: "))
 
 # Tells how many spaces to add
 spaces_needed = width - len(text)
+
+# Adds the spaces to the text
+if spaces_needed > 0:
+    result = text + " " * spaces_needed
+else:
+    result = text
+
+# Displays result
+print("'" + result + "'")
