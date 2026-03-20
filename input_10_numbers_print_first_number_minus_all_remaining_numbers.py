71a26bcde85a5d0b131102126915c5778f57d141
yanab <yanaborja610@gmail.com>
2026-03-20 08:28:07 +0800

    Fo the input of 10 numbers

--- input_10_numbers_print_first_number_minus_all_remaining_numbers.py
+++ input_10_numbers_print_first_number_minus_all_remaining_numbers.py
@@ -1,1 +1,7 @@
 #  Create a program that ask user to input 10 numbers. Print the result of the first number minus all the remaining numbers.
+
+# For input of numbers
+result = float(input("Enter number 1: "))
+
+for i in range(2, 11):
+    result -= float(input(f"Enter number {i}: "))
