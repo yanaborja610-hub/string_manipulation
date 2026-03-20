1f3360bef2e44b4cfc931a878d7301d7304c4df9
yanab <yanaborja610@gmail.com>
2026-03-20 16:36:55 +0800

    For the input of text and desired width/length

--- add_space_at_the_beginning_and_end_of_string_to_make_it_centered.py
+++ add_space_at_the_beginning_and_end_of_string_to_make_it_centered.py
@@ -1,2 +1,6 @@
 # center() add space characters at the beginning and at the end of the string to print the string at the center.
 # Create a program that do the same functionality without using center() function.
+
+# For the input of text and desired width
+text = input("Enter text: ")
+width = int(input("Enter desired width: "))
